5eef809a190a145f7e96ea070fc6eaf9908cbae7
Steven Cobb <sjcobb.dev@gmail.com>
2021-09-11 20:03:35 -0400

    cleanup, change dataset source ex

--- waterfall.js
+++ waterfall.js
@@ -8,14 +8,24 @@
 let waterfallId = document.getElementById('waterfall-1');
 const waterfallChart = echarts.init(waterfallId, 'tech-blue');
 
-const waterfallData = [
-    ['barLabel', 'barStartValue', 'barEndValue'],
-    ['Total', 0, 100],
-    ['Label 1', 20, 100],
-    ['Label 2', -10, 20],
-    ['Label 3', 30, -10],
-    ['Net', 30, 0],
+const waterfallData =  [
+    ['barLabel', 'barStartValue', 'barEndValue', 'referenceDatapoint'],
+    ['Total Assets', 0, 67612],
+    ['Total Liabilities', 67612, -16303],
+    ['Equity', -16303, 7500, 1],
+    ['Interest', 7500, 7500],
+    ['Total Debt', 7500, 48980],
+    ['Value', 48980, 66657, 1],
 ];
+
+// const waterfallData = [
+//     ['barLabel', 'barStartValue', 'barEndValue'],
+//     ['Total', 0, 100],
+//     ['Label 1', 20, 100],
+//     ['Label 2', -10, 20],
+//     ['Label 3', 30, -10],
+//     ['Net', 30, 0],
+// ];
 
 const lastBarIndex = waterfallData.length - 2;
 let hasReferenceData = false;
@@ -41,19 +51,10 @@ const waterfallOption = {
     xAxis: {
         show: true,
         type: 'category',
-        // scale: true,
-        // axisLabel: {
-        //     formatter: (value, index) => {
-        //         return this.axisLabelFormatter(value.toString(), 15);
-        //     },
-        // },
     },
     yAxis: {
         show: true,
         type: 'value',
-        // axisLabel: {
-        //     formatter: label => this.getFormattedUnit(this.config, label, 'yAxis'),
-        // },
     },
     series: [
         {
@@ -66,7 +67,6 @@ const waterfallOption = {
                 fontSize: 10,
                 formatter: params => {
                     const labelData = params.data[2] - params.data[1];
-                    // return this.getFormattedUnit(config, labelData, 'barLabel');
                     return labelData;
                 },
             },
@@ -91,38 +91,29 @@ const waterfallOption = {
                 let rectHeight = startCoord[1] - endCoord[1];
 
                 const style = api.style();
-                // style.fill = this.chartTheme.color[dataIndex];
                 style.fill = chartThemeColor[dataIndex];
                 if (rectHeight < 0) {
-                    // style.fill = this.chartTheme.negColor;
-                    style.fill = '#FE433C'; // red
+                    style.fill = '#FE433C'; // red (negativeColor)
                 } else if (rectHeight === 0) {
-                    // style.fill = this.chartTheme.neutralValueColor;
-                    style.fill = '#ededed'; // ltgray
+                    style.fill = '#ededed'; // ltgray (neutralValueColor)
                 } else {
-                    // style.fill = this.chartTheme.posColor;
-                    style.fill = '#0095EF'; // blue
+                    style.fill = '#0095EF'; // blue (positiveColor)
                 }
 
                 const referenceData = api.value(3);
                 if (referenceData === 1) {
-                    // style.fill = this.chartTheme.refColor;
-                    style.fill = '#228B22'; // green
+                    style.fill = '#228B22'; // green (referenceColor)
                     hasReferenceData = true;
                 } else if (referenceData === 0) {
-                    // style.fill = this.chartTheme.nonRefColor;
-                    style.fill = '#808080'; // gray
+                    style.fill = '#808080'; // gray (nonReferenceColor)
                 }
 
                 // if referenceDatapoint undefined, set last bar to green reference color
                 if (hasReferenceData === false && dataIndex === lastBarIndex) {
-                    // style.fill = this.chartTheme.refColor;
-                    style.fill = '#228B22'; // green
+                    style.fill = '#228B22'; // green (referenceColor)
                 }
 
-                // set rectMinHeight AFTER style so 0 stays as black neutralValueColor
                 rectHeight = rectHeight === 0 ? rectMinHeight : rectHeight;
-
                 const rectItem = {
                     type: 'rect',
                     shape: {
@@ -133,7 +124,6 @@ const waterfallOption = {
                     },
                     style: style,
                 };
-
                 return rectItem;
             },
         },
@@ -149,152 +139,5 @@ const chartThemeColor = [
     '#A0DCFF',
     '#C3EDFF'
 ];
-
-// const waterfallOption = {
-//     title: {
-//         show: false,
-//         text: 'Circle of Fifths',
-//         left: 'center',
-//         textStyle: {
-//             color: '#333',
-//             fontFamily: '"Roboto Condensed", Verdana, sans-serif',
-//             fontWeight: 600,
-//             fontSize: 16,
-//         },
-//     },
-//     tooltip: {
-//         trigger: 'axis',
-//         axisPointer: {
-//             type: 'shadow',
-//         },
-//     },
-//     legend: {
-//         show: false,
-//         type: 'plain', // 'scroll'
-//         data: ['Octave 0', 'Octave 1', 'Octave 2', 'Octave 3', 'Octave 4', 'Octave 5', 'Octave 6', 'Octave 7', 'Octave 8'],
-//     },
-//     grid: {},
-//     polar: {
-//         center: ['50%', '50%'], 
-//         radius: ['45%', '82%'],
-//     },
-//     radiusAxis: {
-//         type: 'value',
-//         show: false,
-//         axisTick: {
-//             show: false,
-//         },
-//     },
-//     angleAxis: {
-//         type: 'category',
-//         show: true,
-//         data: circleOfFifthsMajorOrderedNotes,
-//         clockwise: true,
-//         startAngle: 105,
-//         splitArea: {
-//             show: false,
-//         },
-//         axisLine: {
-//             show: false,
-//         },
-//         axisTick: {
-//             show: false,
-//         },
-//         axisLabel: {
-//             interval: 0,
-//             margin: 10,
-//             color: '#234468',
-//             color: '#ffffff',
-//             fontFamily: '"Roboto Condensed", Verdana, sans-serif',
-//             fontWeight: 600,
-//             fontSize: 44,
-//             formatter: (value, index) => {
-//                 // ['C', 'G', 'D', 'A', 'E', 'B', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F'];
-//                 // ['C', 'G', 'D', 'A', 'E', 'B', 'G♭', 'D♭', 'A♭', 'E♭', 'B♭', 'F']; // undef ♭
-//                 // ['C', 'G', 'D', 'A', 'E', 'B', 'F♯', 'C♯', 'G♯', 'D♯', 'A♯', 'F']; // undef ♯
-//                 switch (value) {
-//                     case 'Gb':
-//                         return 'G♭';
-//                     case 'Db':
-//                             return 'D♭';
-//                     case 'Ab':
-//                             return 'A♭';
-//                     case 'Eb':
-//                         return 'E♭';
-//                     case 'Bb':
-//                         return 'B♭';
-//                     default:
-//                         return value;
-//                 }
-//             }
-//         }
-//     },
-//     series: [
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 0',
-//             stack: 'total',
-//             barCategoryGap: 1,
-//             data: fullCircleData[0],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 1',
-//             stack: 'total',
-//             data: fullCircleData[1],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 2',
-//             stack: 'total',
-//             data: fullCircleData[2],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 3',
-//             stack: 'total',
-//             data: fullCircleData[3],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 4',
-//             stack: 'total',
-//             data: fullCircleData[4],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 5',
-//             stack: 'total',
-//             data: fullCircleData[5],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 6',
-//             stack: 'total',
-//             data: fullCircleData[6],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 7',
-//             stack: 'total',
-//             data: fullCircleData[7],
-//         },
-//         {
-//             type: 'bar',
-//             coordinateSystem: 'polar',
-//             name: 'Octave 8',
-//             stack: 'total',
-//             data: fullCircleData[8],
-//         },
-//     ]
-// };
 
 waterfallChart.setOption(waterfallOption);
